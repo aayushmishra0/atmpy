241bf80f0bc9258f25ae1ed6af14d2913d899624
aayushmishra0 <71607632+aayushmishra0@users.noreply.github.com>
2022-06-11 11:48:31 -0500

    Add files via upload

---
+++ class100.py
@@ -0,0 +1,20 @@
+class Car(object):
+    def __init__(self, model, color, company, speed_limit):
+        self.color = color
+        self.company = company
+        self.speed_limit = speed_limit
+        self.model = model
+
+def start(self):
+    print('started')
+def stop(self):
+    print('stopped')
+def accelarate(self):
+    print('accelaterating...')
+    'accelarator functionality here'
+def change_gear(self, gear_type):
+    print('gear change')
+    'gear related functionality here'
+
+tesla = Car('model Y', 'white', 'telsa', '100')
+print(tesla.stop())
